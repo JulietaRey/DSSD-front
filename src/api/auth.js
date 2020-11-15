bc41f3c3f73ede5ab27823ffb473dd17f94c8d7a
Toto <toto_lp77@hotmail.com>
2020-11-15 13:51:59 -0300

    Today, smoking is going to save lives

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,13 +2,12 @@ const url = 'https://dssd-2020-ytj.herokuapp.com';
 export const signInCall = async (data) => {
   const res = await fetch(`${url}/auth/signin`, {
     method: 'POST',
-    body: JSON.stringify(data),
+    body: new URLSearchParams({
+      'username': data.username,
+      'password': data.password
+    }),
     credentials: 'omit',
-    headers: {
-      // 'Access-Control-Allow-Origin': true,
-      // 'Content-Type': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
+    
   });
 
   console.log(await res.json());
